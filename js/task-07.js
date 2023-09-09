aa35ff38ea14ce7379bcb3ea6acacc97b9ceb207
TaniaUdod <taniaudod28@gmail.com>
2023-09-09 20:41:58 +0300

    Update

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,19 @@
-const textInput = document.querySelector("#font-size-control");
-const textSize = document.querySelector("#text");
+// const textInput = document.querySelector("#font-size-control");
+// const textSize = document.querySelector("#text");
 
-textInput.addEventListener("input", (event) => {
-    textSize.style.fontSize = `${event.currentTarget.value}px`;
+// textSize.style.fontSize = `${textInput.value}px`;
+
+// textInput.addEventListener("input", () => {
+//     textSize.style.fontSize = `${textInput.value}px`;
+// });
+
+const refs = {
+    input: document.querySelector("#font-size-control"),
+    spanText: document.querySelector("#text"),
+};
+
+refs.spanText.style.fontSize = `${refs.input.value}px`;
+
+refs.input.addEventListener("input", () => {
+    refs.spanText.style.fontSize = `${refs.input.value}px`;
 });
